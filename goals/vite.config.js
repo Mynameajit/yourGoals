43920fb7d07f1558a9dd9d3c50b460517a91a22d
Mynameajit <132321923+Mynameajit@users.noreply.github.com>
2025-05-20 15:26:56 +0530

    Add files via upload

--- goals/vite.config.js
+++ goals/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // allow access from LAN or ngrok
-    allowedHosts: 'all', // ✅ allow ANY host (including ngrok)
-  },
+base:'/yourGoals/'
 });
